139cb563de4931beed52bd63c8ca2ae63ed7fef3
nae <email@email.email>
2026-02-27 18:30:54 +0300

    increment messages

--- src/shared/lib/messages.js
+++ src/shared/lib/messages.js
@@ -1,4 +1,4 @@
 export const MSG_COMMANDS = "Доступные команды:\n/list";
-export const MSG_LIST = "Ты нажал на list – молодец! ";
+export const MSG_LIST = `Ты нажал на list – молодец!\n\n${MSG_COMMANDS}`;
 export const MSG_UNVERIFIED = "Тебя скоро добавят, подожди немного.";
-export const MSG_LINK_SAVED = "Ссылка сохранена!";
+export const MSG_LINK_SAVED = `Ссылка сохранена!\n\n${MSG_COMMANDS}`;
